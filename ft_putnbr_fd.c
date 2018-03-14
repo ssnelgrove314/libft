a50306d9385680ec9f52c8c8ac1bbd2cd3e00dd5
Scott Snelgrove <ssnelgro@e1z3r2p21.42.us.org>
2018-03-14 16:18:43 -0700

    I love my annoying keyboard

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ssnelgro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 16:04:03 by ssnelgro          #+#    #+#             */
-/*   Updated: 2018/03/08 00:19:21 by ssnelgro         ###   ########.fr       */
+/*   Updated: 2018/03/14 16:16:33 by ssnelgro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@
 void	ft_putnbr_fd(int nb, int fd)
 {
 	if (nb == -2147483648)
+	{
 		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	if (nb < 0)
 	{
 		ft_putchar_fd('-', fd);
